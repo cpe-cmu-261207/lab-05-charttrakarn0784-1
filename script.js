b87b59766e038181efb263856f162d74d43c9b57
charttrakarn0784 <charttrakarn6@hotmail.co.th>
2022-08-04 16:06:31 +0700

    finished

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const todoCtn = document.getElementById("todo-container");
 
 inputAdd.onkeyup = (event) => {
   if (event.key !== "Enter") return;
-
   //your code here
+  if (inputAdd.value !== "") {
+    addTodo(inputAdd.value, false);
+  } else alert("Todo cannot be empty");
 };
 
 function addTodo(title, completed) {
@@ -31,18 +33,63 @@ function addTodo(title, completed) {
   //your code here
   //append todo to HTML...
   //define buttons event...
+  div.appendChild(span);
+  div.appendChild(doneBtn);
+  div.appendChild(deleteBtn);
+
+  doneBtn.style.display = "none";
+  deleteBtn.style.display = "none";
+  //mouse event
+  div.onmouseout = () => {
+    doneBtn.style.display = "none";
+    deleteBtn.style.display = "none";
+  };
+  div.onmouseover = () => {
+    doneBtn.style.display = "";
+    deleteBtn.style.display = "";
+  };
+  doneBtn.onclick = () => {
+    if (span.style.textDecoration === "line-through") {
+      span.style.textDecoration = "";
+      saveTodo();
+    } else {
+      span.style.textDecoration = "line-through";
+      saveTodo();
+    }
+  };
+  deleteBtn.onclick = () => {
+    todoCtn.removeChild(div);
+    saveTodo();
+  };
+  todoCtn.prepend(div);
+  saveTodo();
 }
 
 function saveTodo() {
   const data = [];
   for (const todoDiv of todoCtn.children) {
     //your code here
+    const todoObj = {};
+    todoObj.title = todoDiv.children[0].innerText;
+    todoObj.completed =
+      todoDiv.children[0].style.textDecoration === "line-through";
+    data.push(todoObj);
   }
   //your code here
+  // console.log(data);
+  const dataStr = JSON.stringify(data);
+  localStorage.setItem("todoListData", dataStr);
 }
 
 function loadTodo() {
   //your code here
+  const dataStr = localStorage.getItem("todoListData");
+  const data = JSON.parse(dataStr);
+  console.log(data);
+  for (const todoObj of data) {
+    // addTodo(todoObj.title, todoObj.completed);
+    console.log(todoObj);
+  }
 }
 
 loadTodo();
